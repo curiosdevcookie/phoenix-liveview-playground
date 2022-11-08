f88b161702586b948fd6d7e80ac9f94d6f23e232
Ariadne Engelbrecht <ariadneengelbrecht@dionysos.local>
2022-11-08 13:05:08 +0100

    Add LiveView for a clock

--- lib/demo_web/live/clock_live.ex
+++ lib/demo_web/live/clock_live.ex
@@ -3,7 +3,7 @@ defmodule DemoWeb.ClockLive do
 
   def mount(_params, _session, socket) do
     if connected?(socket) do
-      Process.send_after(self(), :tick, 1000)
+      Process.send_after(self(), :tick, 3000)
     end
 
     socket = assign(socket, :now, Time.utc_now())
@@ -12,7 +12,20 @@ defmodule DemoWeb.ClockLive do
 
   def render(assigns) do
     ~L"""
-    <h1> The time is <%=@now%> </h1>
+    <h1> The time is <%=@now%>.</h1>
     """
+  end
+
+  def handle_info(:tick, socket) do
+   socket = assign_current_time(socket)
+    {:noreply, socket}
+  end
+
+  def assign_current_time(socket) do
+    now=
+      Time.utc_now()
+      |> Time.to_string()
+      |> String.slice(0..7)
+    assign(socket, :now, now)
   end
 end
